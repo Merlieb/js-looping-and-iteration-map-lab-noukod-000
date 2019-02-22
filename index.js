f9e28663e9376dd0f86b6fea925171a1a5e66fbf
Merline Belizaire <merline@noukod.com>
2019-02-22 20:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,35 +2,35 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map(function(driverName){return driverName.toLowerCase() });
 }
-const drivers = ['Bob Simpson', 'James Smart', 'John Jameson'];
-
-const driversObjects = [];
-  drivers.map(function(driver) {
-    driver.split("")})
-}
-
-function splitName(names) {
-  return names.map(function(name) {
-    return name.split(" ");
-  })
-}
-
-
-const splitNames = splitName(drivers);
-
-function objectConstructor(splitNames) {
-  const driversObjects= [];
-  splitNames.map(function(splitName) {
-    const newObj = {};
-    newObj.firstName = splitName[0];
-    newObj.secondName = splitName[1];
-    return driversObjects.push(newObj);
-  })
-  return driversObjects;
-}
-
-objectConstructor(splitNames);
-
+// const drivers = ['Bob Simpson', 'James Smart', 'John Jameson'];
+//
+// const driversObjects = [];
+//   drivers.map(function(driver) {
+//     driver.split("")})
+// }
+//
+// function splitName(names) {
+//   return names.map(function(name) {
+//     return name.split(" ");
+//   })
+// }
+//
+//
+// const splitNames = splitName(drivers);
+//
+// function objectConstructor(splitNames) {
+//   const driversObjects= [];
+//   splitNames.map(function(splitName) {
+//     const newObj = {};
+//     newObj.firstName = splitName[0];
+//     newObj.secondName = splitName[1];
+//     return driversObjects.push(newObj);
+//   })
+//   return driversObjects;
+// }
+//
+// objectConstructor(splitNames);
+//
 // const drivers = [
 //         { name: 'Bobby',   hometown: 'Pittsburgh'  },
 //         { name: 'Sammy',   hometown: 'New York'    },
