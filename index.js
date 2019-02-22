815b419e815cfd122bfd4394a4a1147e9f907140
Merline Belizaire <merline@noukod.com>
2019-02-22 20:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+const drivers = [
+        { name: 'Bobby',   hometown: 'Pittsburgh'  },
+        { name: 'Sammy',   hometown: 'New York'    },
+        { name: 'Sally',   hometown: 'Cleveland'   },
+        { name: 'Annette', hometown: 'Los Angeles' },
+        { name: 'Bobby',   hometown: 'Tampa Bay'   }
+      ];
+
+function attributesToPhrase(drivers) {
+  const strings = [];
+  drivers.map(function(driver) {
+    strings.push(`${driver.name} is from ${driver.hometown}`)
+  })
+  return strings;
+}
+
+attributesToPhrase(drivers);
