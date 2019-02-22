38a423a5cf438077774d44bee4ff5e35d75d2ccf
Merline Belizaire <merline@noukod.com>
2019-02-22 20:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function lowerCaseDrivers(drivers) {
     return drivers.map(function(driverName){return driverName.toLowerCase() });
   }
 
+  function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return {firstName: driverFirst, lastName: driverLast};
+  })
+};
+
 function attributesToPhrase(drivers) {
   const strings = [];
   drivers.map(function(driver) {
